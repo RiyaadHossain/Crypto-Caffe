5a8b57e8a9efb8dfd0c388d322cadac90c493ebf
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-02 16:35:47 +0600

    Explore Coins ~ button fixed

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Homepage = () => {
+  const coin = useNavigate()
   return (
     <div className='md:px-16 py-8'>
       <div className='flex flex-col justify-center h-[80vh] items-center'>
         <h1 className=' font-mono text-xl md:text-5xl '>
           Welcome to CRYPTO CAFE<span className='text-6xl'>☕</span>
         </h1>
-        <button className='py-2 px-3 text-white mt-3 rounded-full bg-cyan-600'>
+        <button onClick={() => coin('/coins')} className='py-2 px-3 text-white mt-3 rounded-full bg-cyan-600'>
           Explore Coins
         </button>
       </div>
