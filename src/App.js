c40d90c16435dce46180efeda722a559745bcfa4
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-02 16:04:52 +0600

    Header Section complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import About from './components/About/About'
+import Coins from './components/Coins/Coins'
+import Contact from './components/Contact/Contact'
+import Header from './components/Header/Header'
+import Homepage from './components/Homepage/Homepage'
 
 const App = () => {
   return (
     <div>
-      <h1>Crypto Cafe Starter Template</h1>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Homepage/> }/>
+        <Route path='/coins' element={<Coins/> }/>
+        <Route path='/contact' element={ <Contact/>}/>
+        <Route path='/about' element={<About/> }/>
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import CustomLink from "../CustomLink/CustomLink";
+
+const Header = () => {
+  return (
+    <div className="bg-cyan-600">
+      <div className="flex justify-between container mx-auto py-5 ">
+        <div className="logo font-bold text-3xl font-mono">Crypto Caffe</div>{" "}
+        <nav className="flex  font-bold">
+          <CustomLink className='px-3 text-lg' to={"/"}>Home</CustomLink>
+          <CustomLink className='px-3 text-lg' to={"/coins"}>Coins</CustomLink>
+          <CustomLink className='px-3 text-lg' to={"/contact"}>Contact</CustomLink>
+          <CustomLink className='px-3 text-lg' to={"/about"}>About</CustomLink>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
